ca15dcac19ac77bf7292a637ee16dacde4079572
amin <amin.w3dev@gmail.com>
2011-12-24 18:32:03 +0330

    Version 1.1 - first commit

---
+++ Phprop.php
@@ -0,0 +1,175 @@
+<?php
+/**
+ * PHProp
+ *
+ * PHP Version 5.3
+ * @author Amin Saeedi <amin.w3dev@gmail.com>
+ * @copyright Copyright (c) 2009-2011. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GPL v3.0
+ * @version 1.1
+ */
+
+/**
+ * PHPRop 
+ * Ini configuration parser with dependant variable support 
+ * 
+ * @package Configuration
+ * @author Amin Saeedi <amin.w3dev@gmail.com>
+ * @copyright Copyright (C) 2009-2011. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GPL v3.0
+ * @version 1.1
+ */
+class PHPRop 
+{
+    /**
+     * @var IniObject
+     */
+    private $_obj;
+
+    /**
+     * @var mixed
+     */
+    private $_tmpValue;
+
+    /**
+     * @var string
+     */
+    private $_delimiter;
+
+    /**
+     * @var array
+     */
+    private $_varDeps = array();
+    private static $_instance = null;
+
+    /**
+     * Initialization
+     *
+     * @param string ini filename
+     * @param string delimiter
+     */
+    private function  __construct($iniFile, $delimiter) 
+    {
+        $this->_delimiter = $delimiter;
+        $this->_obj = new iniObject();
+        $this->_parseIni($iniFile);
+    }
+
+    /**
+     * Singleton pattern constructor
+     *
+     * @param string ini filename
+     * @param string delimiter, it is '.' by default but can be changed
+     * @return object : object containing ini data
+     */
+    public static function parse($filename, $del=".")
+    {
+        if (is_null(self::$_instance)) {
+            self::$_instance = new self($filename, $del);
+        }
+        return self::$_instance->getObj();
+        //return self::$_instance;
+    }
+
+    /**
+     * Parses ini file and extract its keys and prepare it for object creation
+     */
+    private function _parseIni($iniFile)
+    {
+        $aParsedIni = parse_ini_file($iniFile, true, INI_SCANNER_RAW);
+        $tmpArray = array();
+        foreach ($aParsedIni as $key=>$value) { 
+            if (!is_array($value)) {
+                break;
+            } else {
+                foreach ($value as $vk=>$vv) {
+                    $newKey = $key.".".$vk;
+                    $tmpArray[$newKey] = $vv;
+                    if (preg_match_all('/\${([a-zA-Z0-9\.]+)}/', $vv, $match)) {
+                        if (!isset($match[1])) continue;
+                        $variableKey = $match[1];
+                        foreach ($variableKey as &$var) {
+                            if (strpos($var, '.') === false) {
+                                $var = $key . '.' . $var;
+                            }
+                        }
+                        $this->_varDeps[$newKey] = $variableKey;
+                    }
+                }
+            }
+        }
+		if (!empty($tmpArray)) {
+			$aParsedIni = $tmpArray;
+		}
+        foreach ($aParsedIni as $key=>$value) { 	        //extract parsed array keys
+            if (array_key_exists($key, $this->_varDeps)) {
+                $deps = &$this->_varDeps;
+                $value = preg_replace_callback('/\${([a-zA-Z0-9\.]+)}/', 
+                        function($match) use ($key, $aParsedIni, &$deps){
+                            return $aParsedIni[array_shift($deps[$key])];
+                        }, $value);
+                $aParsedIni[$key] = $value;
+            }
+            $this->_tmpValue = $value;         			//set temporay value to current ini value
+            $aXKey = explode($this->_delimiter, $key); 	//get ini key segments
+            $this->_recursiveInit($this->_obj, $aXKey); //set object properties recursively based on parsed ini
+        }
+    }
+
+    /**
+     *
+     * @param object Parent node
+     * @param object Child Node
+     */
+    private function _recursiveInit($objParent, array $aChilds)
+    {
+        $child = $aChilds[0];
+        if (count($aChilds) > 1) {
+            if (!isset($objParent->$child)) {
+                $objParent->$child = new iniObject();
+            }
+            array_shift($aChilds);  //drop first element of child array
+            $this->_recursiveInit($objParent->$child, $aChilds);
+        } else {
+            $objParent->$child = $this->_tmpValue;   //set the last child to temporary value
+        }
+    }
+
+    /**
+     * ini object getter
+     *
+     * @return object : object created from ini file
+     */
+    public function getObj()
+    {
+        return $this->_obj;
+    }
+}
+
+/**
+ * Customized stdClass
+ */
+class iniObject implements Countable, ArrayAccess
+{
+    public function count()
+    {
+        return count( get_object_vars($this) );
+    }
+    
+    public function offsetSet($offset, $value) 
+    {
+        $this->$offset = $value;
+    }
+    public function offsetExists($offset) 
+    {
+        return isset($this->$offset);
+    }
+    public function offsetUnset($offset) 
+    {
+        unset($this->$offset);
+    }
+    public function offsetGet($offset) 
+    {
+        return isset($this->$offset) ? $this->$offset : null;
+    }
+}
---
+++ README.md
@@ -0,0 +1,50 @@
+#Description
+One common configuration format that is used in PHP apps are ini files, but ini files are being used mostly for non-hierarchical configurations. For hierarchical data structure, XML is more used.
+This package facilitates parsing and reading hierarchical data from ini files.
+To create a hierarchical ini file, the only thing to do is to write keys seperated by a delimiter
+(by default '.') to specify parent and child. For example:
+
+    application.title="title"
+    application.db.username="username"
+    application.db.password="123"
+
+The example above can be written like this:
+
+    [application]
+    title="title"
+    db.username="username"
+    db.password="123"
+
+You can also use a value by it's key in another configuration value:
+
+    [application]
+    url=http://localhost/my-app
+    login=${url}/login
+
+Note: ${url} assumes *url* key is in the current section, it can be written like ${application.url}.
+If you want to refer to ${url} in another section (e.g. global), you should mention it as a prefix e.g. ${global.url}.  
+If you are using multi-level configuration keys (e.g. application.config.db.username), write the root as prefix, e.g. ${*prefix*.variable}.
+
+#Features
+- Easy to use
+- Convenient Integration
+- Support for hierarchical data structure
+- Key-value binding
+
+##How to use
+Pass the ini configuration path to the *parse* method:
+    $ini = PHProp::parse("path/to/ini");
+
+If your scope delimiter is not '.', give your delimiter as the second parameter:
+    $ini = PHProp::parse("path/to/ini", "/");
+
+After getting the object, the username can be accessed easily:
+    $ini->application->db->username 
+
+or
+    $ini['application']['db']['username']
+
+To get the number of application's children:
+    count($ini->application);
+
+See the examples for more...
